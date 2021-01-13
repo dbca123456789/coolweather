4a1f65dab4805dcfdf6441ddf2ac1d3c6086bc68
admin <921623221@qq.com>
2021-01-13 16:32:16 +0800

    加入创建数据库和各项配置

---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,31 @@
+package com.example.coolweather.db;
+
+public class County {
+    private int id;
+    private String countyName;
+    private int countyCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(int countyCode) {
+        this.countyCode = countyCode;
+    }
+}
