fb2665a029f52cb4f57ecae1fa8f03b2a196d6b7
admin <921623221@qq.com>
2021-01-15 10:50:31 +0800

    显示天气信息

--- app/src/main/java/com/example/coolweather/db/County.java
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -5,10 +5,10 @@ import org.litepal.crud.DataSupport;
 public class County extends DataSupport {
     private int id;
     private String countyName;
-    private int countyCode;
+
 
     private int cityId;
-
+    private String weatherId;
 
     public int getCityId() {
         return cityId;
@@ -34,11 +34,11 @@ public class County extends DataSupport {
         this.countyName = countyName;
     }
 
-    public int getCountyCode() {
-        return countyCode;
+    public String  getWeatherId() {
+        return weatherId;
     }
 
-    public void setCountyCode(int countyCode) {
-        this.countyCode = countyCode;
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
     }
 }
